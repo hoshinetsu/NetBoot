516ce1193bd6ce619b94663289716e335c067dd0
hoshinetsu <62859523+hoshinetsu@users.noreply.github.com>
2025-03-22 09:55:01 +0100

    ping api improvements

--- api/ping.php
+++ api/ping.php
@@ -1,14 +1,17 @@
 <?php
 error_reporting(0);
 $out = array();
-$result = exec("ping -n 2 -w 250 172.31.69.95", $out);
+$result = exec("ping -n 2 -w 250 172.16.6.6", $out);
 $up = false;
-foreach($out as $line){
+echo "<stdout>";
+foreach ($out as $line) {
     echo $line . "<br>";
-    if(str_contains($line, "time=")) {
+    if (str_contains($line, "time=")) {
         $up = true;
         break;
     }
 }
-echo "<br>HOST IS " . ($up ? "up" : "down");
+echo "</stdout><up>";
+echo $up ? 1 : 0;
+echo "</up>";
 ?>
